7ec653989fc41fd945c0dce80a8299692ec36077
Danidu <danduhansaka@gmail.com>
2026-04-03 00:09:45 +0530

    Add new screenshots and update F_Section component to use new images

--- frontend/src/components/Features Components/F_Section.jsx
+++ frontend/src/components/Features Components/F_Section.jsx
@@ -1,7 +1,12 @@
 import "./F_Section.css"
 import Button from "../Shared/Button"
 import ContentBox from "../Shared/ContentBox";
-import BoxImg1 from "../../assets/Screenshot.png"
+import BoxImg1 from "../../assets/Screenshot1.png"
+import BoxImg2 from "../../assets/Screenshot2.png"
+import BoxImg3 from "../../assets/Screenshot3.jpg"
+import BoxImg4 from "../../assets/Screenshot4.jpg"
+import BoxImg5 from "../../assets/Screenshot5.jpg"
+import BoxImg6 from "../../assets/Screenshot6.jpg"
 
 export default function F_Section(){
     return(
@@ -14,23 +19,23 @@ export default function F_Section(){
 
                 <ContentBox heading="Extraction" span="Automated" paragraph="Analyzes source code using 
                             static program analysis to extract functions, classes, variables, and execution
-                            flow." image={BoxImg1}/>
+                            flow." image={BoxImg4}/>
 
                 <ContentBox heading="Visualization" span="Smart" paragraph="Adjusts detail based on zoom level,
                             presenting architecture views when zoomed out and function level detail."
-                             image={BoxImg1}/>
+                             image={BoxImg2}/>
 
                 <ContentBox heading="Interaction" span="Dual-Pane" paragraph="Provides bidirectional synchronization 
                             between flowchart elements and source code, enabling users to trace logic." 
-                            image={BoxImg1}/>
+                            image={BoxImg3}/>
 
                 <ContentBox heading="Reasoning" span="AI-Powered" paragraph="Integrates conversational AI that 
                             provides natural language explanations and targeted insights tied to specific code."   
-                            image={BoxImg1}/>
+                            image={BoxImg5}/>
 
                 <ContentBox heading="Exploration" span="Guided" paragraph="Supports a guided walkthrough of 
                             program execution, enabling users to follow control flow paths for understanding." 
-                            image={BoxImg1}/>
+                            image={BoxImg6}/>
                 
                 
             </div>
